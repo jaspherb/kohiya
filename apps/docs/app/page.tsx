68efb190d4033fd3b33c7d9a32930d3872bafc15
jaspherb <japbaldera@gmail.com>
2024-09-28 02:30:52 +0800

    feat(apps/docs): :sparkles: add next-theme mode toggle

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,9 +1,13 @@
-import { Button } from "@/components/ui/button";
+import { ModeToggle } from '@/components/ui/mode-toggle';
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="w-full min-h-screen">
+      <div className="container">container</div>
+      {/* To center a container, use the mx-auto utility: */}
+      <div className="container mx-auto">
+        <ModeToggle></ModeToggle>
+      </div>
     </div>
   );
 }
