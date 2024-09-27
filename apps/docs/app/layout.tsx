61a214cc16554a0254e985168fc6259f46615220
jaspherb <japbaldera@gmail.com>
2024-09-28 04:50:32 +0800

    feat(apps/docs): add shadcn

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,32 +1,35 @@
-import type { Metadata } from "next";
-
-// These styles apply to every route in the application
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Kohiya",
-  description: "",
+  title: 'kohiya',
+  description: 'Discover the best coffee and experience the charm of Kohiya.'
 };
 
 type RootLayoutProps = {
   children: React.ReactNode;
 };
 
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from '@/components/theme-provider';
+import { AppBar } from '@/components/app-bar';
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
-        <head />
-        <body>
+        <body className="bg-white dark:bg-black text-black dark:text-white">
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
-            {children}
+            <div className="max-h-screen flex flex-col">
+              <header className="fixed top-0 left-0 right-0 z-50 p-3  bg-white dark:bg-black text-black dark:text-white">
+                <AppBar />
+              </header>
+              <main className="container mx-auto p-6 pt-16">{children}</main>
+            </div>
           </ThemeProvider>
         </body>
       </html>
--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
     darkMode: ['class'],
     content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}', // For the App Router in Next.js 13+
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
   	extend: {
